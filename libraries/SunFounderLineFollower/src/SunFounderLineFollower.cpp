cbedaf4f8ef03a087d3287596bcf4e7802c8fc13
Txinto Vaz <txinto@gatatac.org>
2018-04-16 17:32:22 +0100

    We delete the Wire initialization in order to make it work with another Wire devices

--- libraries/SunFounderLineFollower/src/SunFounderLineFollower.cpp
+++ libraries/SunFounderLineFollower/src/SunFounderLineFollower.cpp
@@ -5,14 +5,14 @@
 
 SunFounderLineFollower::SunFounderLineFollower()
 {
-	
+
 	//SDA=D2 and SCL=D1
     //Wire.begin(D1,D2);
-    Wire.begin();
+    //Wire.begin();
     //Calibration Min and Max
     int  cal_max[8];
     int  cal_min[8];
-      
+
 }
 
 void SunFounderLineFollower::calibrate()
@@ -22,10 +22,10 @@ void SunFounderLineFollower::calibrate()
     for (int x=0; x <= 100; x++) {
 	for (int i=0; i <= 7; i++) {
 	  raw[i] = Wire.read() << 8 | Wire.read();
-	  
+
 	  if (cal_min[i] < 1){
 		  cal_min[i] = raw[0];
-	   }	  
+	   }
 	  cal_min[i] = ((raw[i] > 0) && (raw[i] < cal_min[i])) ? raw[i] : cal_min[i];
 	  cal_max[i] = (raw[i] > cal_max[i]) ? raw[i] : cal_max[i];
 	}
@@ -37,29 +37,29 @@ void SunFounderLineFollower::calibrate_show()
   Serial.print("cal_show min\t");
   	for (int i=0; i <= 7; i++) {
 	    Serial.print(cal_min[i]);
-        Serial.print("\t"); 
-	}    
+        Serial.print("\t");
+	}
 	Serial.println("");
 
   Serial.print("cal_show max\t");
   	for (int i=0; i <= 7; i++) {
 	    Serial.print(cal_max[i]);
-        Serial.print("\t"); 
+        Serial.print("\t");
 	}
-	Serial.println("");    
+	Serial.println("");
  }
 
 void SunFounderLineFollower::calibrate_reset()
 {
-  	for (int i=0; i <= 7; i++) { 
-		cal_min[i]=0; 
+  	for (int i=0; i <= 7; i++) {
+		cal_min[i]=0;
 		cal_max[i]=0;
 	}
  }
 
 void SunFounderLineFollower::test()
 {
-  Serial.print("test \t"); 
+  Serial.print("test \t");
   int  r[8];
   unsigned char _data[16];
   int reqlen=Wire.requestFrom(9, 16);    // request 16 bytes from slave device #9
@@ -68,7 +68,7 @@ void SunFounderLineFollower::test()
     _data[i+1] = Wire.read(); // receive a byte as character
     r[i/2] = _data[i] << 8 | _data[i+1];
     Serial.print(r[i/2]);
-    Serial.print("\t"); 
+    Serial.print("\t");
   }
   Serial.println("");
 }
